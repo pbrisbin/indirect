7f58b56954e2669e7d3afbe742a8d23323af04ef
patrick brisbin <pbrisbin@gmail.com>
2024-11-13 20:52:28 -0500

    Flesh out more

--- src/Indirect/Config.hs
+++ src/Indirect/Config.hs
@@ -1,11 +1,71 @@
 module Indirect.Config
   ( Config (..)
+  , Executable (..)
   , load
   ) where
 
 import Indirect.Prelude
 
-data Config = Config
+import Control.Exception (Exception, throwIO)
+import Data.Bitraversable (bimapM)
+import Data.Map.Monoidal.Strict (MonoidalMap)
+import Data.Map.Monoidal.Strict qualified as MonoidalMap
+import Data.Map.Strict (Map)
+import Data.Map.Strict qualified as Map
+import Data.Semigroup.Generic
+import System.Environment.XDG.BaseDir (getUserConfigDir)
+import System.FilePath ((<.>), (</>))
+
+newtype Config = Config
+  { unwrap :: Map String Executable
+  }
+
+data Executable = Executable
+  { binary :: Path Abs File
+  , install :: Maybe String
+  }
 
 load :: IO Config
-load = undefined
+load = do
+  xdg <- getUserConfigDir "indirect"
+  raw <-
+    (<>)
+      <$> loadRawConfig (xdg </> "indirect" <.> "toml")
+      <*> loadRawConfig (".indirect" <.> "toml")
+
+  either throwIO pure $ resolveConfig raw
+
+newtype ConfigError = ConfigError
+  { unwrap :: Text
+  }
+  deriving stock (Show)
+  deriving anyclass (Exception)
+
+resolveConfig :: RawConfig -> Either ConfigError Config
+resolveConfig =
+  fmap (Config . Map.fromList)
+    . traverse (secondM resolveExecutable)
+    . MonoidalMap.toList
+    . (.unwrap)
+
+resolveExecutable :: RawExecutable -> Either ConfigError Executable
+resolveExecutable = undefined
+
+newtype RawConfig = RawConfig
+  { unwrap :: MonoidalMap String RawExecutable
+  }
+  deriving newtype (Semigroup)
+
+data RawExecutable = RawExecutable
+  { vars :: MonoidalMap Text (Last String)
+  , binary :: Last String
+  , install :: Maybe (Last String)
+  }
+  deriving stock (Generic)
+  deriving (Semigroup) via (GenericSemigroupMonoid RawExecutable)
+
+loadRawConfig :: FilePath -> IO RawConfig
+loadRawConfig = undefined
+
+secondM :: Applicative f => (b -> f c) -> (a, b) -> f (a, c)
+secondM = bimapM pure
--- src/Indirect/Prelude.hs
+++ src/Indirect/Prelude.hs
@@ -2,4 +2,11 @@ module Indirect.Prelude
   ( module X
   ) where
 
+import Control.Monad as X (guard)
+import Data.Foldable as X (for_)
+import Data.Semigroup as X (Last (..))
+import Data.Text as X (Text, pack, unpack)
+import Data.Traversable as X (for)
+import GHC.Generics as X (Generic)
+import Path as X (Abs, Dir, File, Path, Rel, toFilePath)
 import Prelude as X
