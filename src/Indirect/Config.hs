7d539a124354909562e5d97f9d42010fd29a59ce
patrick brisbin <pbrisbin@gmail.com>
2024-11-13 20:12:15 -0500

    Sketch out Indirect.Main

---
+++ src/Indirect/Config.hs
@@ -0,0 +1,11 @@
+module Indirect.Config
+  ( Config (..)
+  , load
+  ) where
+
+import Indirect.Prelude
+
+data Config = Config
+
+load :: IO Config
+load = undefined
---
+++ src/Indirect/Executable.hs
@@ -0,0 +1,10 @@
+module Indirect.Executable
+  ( findExecutable
+  ) where
+
+import Indirect.Prelude
+
+import Indirect.Config (Config)
+
+findExecutable :: Config -> String -> Maybe FilePath
+findExecutable = undefined
