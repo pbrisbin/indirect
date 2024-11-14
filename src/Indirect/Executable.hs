e1429a3e941c941a5433492811334e9086ab576f
patrick brisbin <pbrisbin@gmail.com>
2024-11-13 22:47:08 -0500

    Add AGPLv3 license boilerplate

--- src/Indirect/Executable.hs
+++ src/Indirect/Executable.hs
@@ -1,3 +1,11 @@
+-- |
+--
+-- Module      : Indirect.Executable
+-- Copyright   : (c) 2024 Patrick Brisbin
+-- License     : AGPL-3
+-- Maintainer  : pbrisbin@gmail.com
+-- Stability   : experimental
+-- Portability : POSIX
 module Indirect.Executable
   ( findExecutable
   ) where
--- src/Indirect/Orphans.hs
+++ src/Indirect/Orphans.hs
@@ -1,3 +1,11 @@
+-- |
+--
+-- Module      : Indirect.Orphans
+-- Copyright   : (c) 2024 Patrick Brisbin
+-- License     : AGPL-3
+-- Maintainer  : pbrisbin@gmail.com
+-- Stability   : experimental
+-- Portability : POSIX
 {-# OPTIONS_GHC -Wno-orphans #-}
 
 module Indirect.Orphans () where
