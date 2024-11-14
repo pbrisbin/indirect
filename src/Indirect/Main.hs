eaa053b9b052ce46f73bd2f9ac29a7a6120dc656
patrick brisbin <pbrisbin@gmail.com>
2024-11-13 19:58:50 -0500

    Package skeleton

---
+++ src/Indirect/Main.hs
@@ -0,0 +1,8 @@
+module Indirect.Main
+  ( main
+  ) where
+
+import Indirect.Prelude
+
+main :: IO ()
+main = putStrLn "Hello world"
---
+++ src/Indirect/Prelude.hs
@@ -0,0 +1,5 @@
+module Indirect.Prelude
+  ( module X
+  ) where
+
+import Prelude as X
