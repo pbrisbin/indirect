7f12b857c22f7482cce91ea69e8ea3fcbd3fbfe9
patrick brisbin <pbrisbin@gmail.com>
2024-11-14 10:28:35 -0500

    Locate .indirect.toml in parents too

--- src/Indirect/Config/Load.hs
+++ src/Indirect/Config/Load.hs
@@ -15,15 +15,37 @@ import Indirect.Prelude
 import Indirect.Config
 import Indirect.Config.Raw
 import Indirect.Config.Resolve
+import System.Directory (doesFileExist, getCurrentDirectory)
 import System.Environment.XDG.BaseDir (getUserConfigDir)
-import System.FilePath ((<.>), (</>))
+import System.FilePath (takeDirectory, (<.>), (</>))
 
 loadConfig :: IO Config
 loadConfig = do
-  xdg <- getUserConfigDir "indirect"
+  user <- getUserConfig
+  proj <- getProjectConfig
   raw <-
     (<>)
-      <$> loadRawConfig (xdg </> "indirect" <.> "toml")
-      <*> loadRawConfig (".indirect" <.> "toml")
+      <$> loadRawConfig user
+      <*> maybe (pure mempty) loadRawConfig proj
 
   resolveConfig raw
+
+getUserConfig :: IO FilePath
+getUserConfig = do
+  xdg <- getUserConfigDir "indirect"
+  pure $ xdg </> "indirect" <.> "toml"
+
+getProjectConfig :: IO (Maybe FilePath)
+getProjectConfig = locateInParents $ ".indirect" <.> "toml"
+
+locateInParents :: FilePath -> IO (Maybe FilePath)
+locateInParents path = go =<< getCurrentDirectory
+ where
+  go cwd = do
+    let absPath = cwd </> path
+
+    exists <- doesFileExist absPath
+
+    if exists
+      then pure $ Just absPath
+      else if cwd == "/" then pure Nothing else go $ takeDirectory cwd
