5b1246f3d2df027a6fd00d15b957891d7aa45923
patrick brisbin <pbrisbin@gmail.com>
2024-11-13 22:47:08 -0500

    Add AGPLv3 license boilerplate

--- src/Indirect/Prelude.hs
+++ src/Indirect/Prelude.hs
@@ -1,3 +1,11 @@
+-- |
+--
+-- Module      : Indirect.Prelude
+-- Copyright   : (c) 2024 Patrick Brisbin
+-- License     : AGPL-3
+-- Maintainer  : pbrisbin@gmail.com
+-- Stability   : experimental
+-- Portability : POSIX
 module Indirect.Prelude
   ( module X
   , secondM
